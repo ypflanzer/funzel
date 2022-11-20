7474dea24f38a444d53417ceedadd5ac4c637389
Yannick Pflanzer <yp1995@outlook.com>
2022-11-20 09:40:30 +0100

    Use curl instead of wget for downloading MNIST.

--- examples/download_mnist.sh
+++ examples/download_mnist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-wget https://github.com/myleott/mnist_png/raw/master/mnist_png.tar.gz
+curl -L -# -o mnist_png.tar.gz https://github.com/myleott/mnist_png/raw/master/mnist_png.tar.gz
 tar -xf mnist_png.tar.gz
