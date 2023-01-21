3bb43d99f4f92158dceec5a4b993054e48c454b9
Yannick Pflanzer <ypflanzer@outlook.com>
2023-01-21 13:07:35 +0100

    Added documentation for Funzel.hpp.

--- libfunzel/include/funzel/Funzel.hpp
+++ libfunzel/include/funzel/Funzel.hpp
@@ -39,6 +39,12 @@
 	#endif
 #endif
 
+/**
+ * @function FUNZEL_REGISTER_BACKEND(name, type)
+ * @brief Registers a backend class.
+ * @param name The name of the backend.
+ * @param type The C++ typename of the backend class.
+ */
 #define FUNZEL_REGISTER_BACKEND(name, type)\
 struct EXPORT StaticInitializer##type \
 {\
@@ -51,22 +57,47 @@ namespace funzel
 {
 
 #ifndef SWIG
-const std::string EmptyStr;
+const std::string EmptyStr; ///< An empty string.
+
+/**
+ * @brief Throws an exception with the given message.
+ * @param msg A message string.
+ */
 [[noreturn]] inline void ThrowError(const std::string& msg)
 {
 	throw std::runtime_error(msg);
 }
 
+/**
+ * @function AssertExcept(v, msg)
+ * @brief Thros an exception if the given predicate is false.
+ * @param v A truth value.
+ * @param msg An error message.
+ */
 #define AssertExcept(v, msg) if(!(v)) { std::stringstream msgss; msgss << msg; funzel::ThrowError(msgss.str()); }
 #endif
 
 class BackendTensor;
+
+/**
+ * @brief Provides an interface for a factory producing BackendTensors.
+ */
 struct ITensorFactory
 {
 	virtual ~ITensorFactory() = default;
+
+	/**
+	 * @brief Creates a new BackendTensor with the given configuration arguments.	 * 
+	 * @param args Some implementation defined configuration string.
+	 * @return std::shared_ptr<BackendTensor> A new BackendTensor.
+	 */
 	virtual std::shared_ptr<BackendTensor> create(const std::string& args = EmptyStr) = 0;
 };
 
+/**
+ * @brief Implements an ITensorFactory for sub-classes of BackendTensor.
+ * @tparam Backend The BackendTensor sub-class to return.
+ */
 template<typename Backend>
 struct TensorFactory : public ITensorFactory
 {
@@ -76,23 +107,69 @@ struct TensorFactory : public ITensorFactory
 	}
 };
 
+/**
+ * @brief Bundles important information about a general compute device.
+ */
 struct DeviceProperties
 {
 	std::string vendorName, deviceName, deviceID;
-	uint64_t memorySize;
+	uint64_t memorySize; ///< The memory size in bytes.
 	bool isGPU;
 };
 
+/**
+ * @brief Gets all available compute devices.
+ * @return The DeviceProperties for all available devices.
+ */
 FUNZEL_API std::vector<DeviceProperties> GetDevices();
+
+/**
+ * @brief Prints a table of available compute devices.
+ */
 FUNZEL_API void PrintDevices();
+
+/**
+ * @brief Get the default backend name string.
+ * @return The default string used to configure new BackendTensor's.
+ */
 FUNZEL_API std::string GetDefaultBackend();
 
 namespace backend
 {
+/**
+ * @brief Registers a new tensor factory.
+ * @attention The pointer needs to remain valid for the entire runtime of the program!
+ * @param name The name of the backend.
+ * @param factory A backend factory object.
+ */
 FUNZEL_API void RegisterTensorBackend(const std::string& name, ITensorFactory* factory);
+
+/**
+ * @brief Creates a new BackendTensor object given a device configuration string.
+ * 
+ * The name string is formatted like this: "$DEVICE:$OPTIONS".
+ * For example, the first OpenCL device may be used with "OCL:0".
+ * See the backend documentation for available options.
+ * 
+ * @param name The device name and config string.
+ * @return A new BackendTensor object.
+ */
 FUNZEL_API std::shared_ptr<BackendTensor> CreateBackendTensor(const std::string& name);
+
+/**
+ * @brief Registers a new device with given properties for use.
+ * @param props The propeties of the new device.
+ */
 FUNZEL_API void RegisterDevice(const DeviceProperties& props);
 
+/**
+ * @brief Loads a backend from a shared library.
+ * 
+ * The right library suffix will be appended automatically for
+ * supported platforms (i.e. Linux, macOS and Windows).
+ * 
+ * @param name The name of the library.
+ */
 FUNZEL_API void LoadBackend(const std::string& name);
 }
 
