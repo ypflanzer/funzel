e840c46f31341fc722dfaf52158a7276c05e4c6c
Yannick Pflanzer <ypflanzer@outlook.com>
2023-01-21 12:52:03 +0100

    Documentation for CVBackendTensor.

--- libfunzel/include/funzel/cv/CVBackendTensor.hpp
+++ libfunzel/include/funzel/cv/CVBackendTensor.hpp
@@ -20,12 +20,32 @@
 
 namespace funzel::cv
 {
+/**
+ * @brief Defines the interface for backend specific computer vision functionality.
+ */
 class CVBackendTensor
 {
 public:
 	virtual ~CVBackendTensor() = default;
+
+	/**
+	 * @brief Converts an image Tensor to grayscale.
+	 * 
+	 * @param self The source Tensor.
+	 * @param tgt The target Tensor.
+	 */
 	virtual void convertGrayscale(const Tensor& self, Tensor tgt) { ThrowError("Operation is not supported!"); }
 
+	/**
+	 * @brief Applies a 2D convolution to an image Tensor.
+	 * 
+	 * @param self The input image Tensor.
+	 * @param tgt The target image Tensor.
+	 * @param kernel A convolution kernel of shape (H, W, C).
+	 * @param stride The stride of the kernel while moving over the image.
+	 * @param padding The padding applied at the edges of the images.
+	 * @param dilation The dilation of the individual kernel elements.
+	 */
 	virtual void conv2d(
 			const Tensor& self, Tensor tgt,
 			const Tensor& kernel,
