fd51cd4014078ab00cd1301eee42e3d7074d8b44
Yannick Pflanzer <yp1995@outlook.com>
2023-05-16 23:20:19 +0200

    Added skeleton for computation graphs.

---
+++ libfunzel/include/funzel/nn/Graph.hpp
@@ -0,0 +1,46 @@
+/* 
+ * This file is part of Funzel.
+ * Copyright (c) 2022 Yannick Pflanzer.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "Module.hpp"
+
+namespace funzel
+{
+namespace nn
+{
+
+template<typename T>
+class FUNZEL_API GraphNode : public T
+{
+public:
+};
+
+class FUNZEL_API Graph : public Module
+{
+public:
+	Graph() = default;
+
+	Tensor forward(const Tensor& input) final override;
+	Tensor backward(const Tensor& input) final override;
+	void to(const std::string& device = EmptyStr) final override;
+	void defaultInitialize() final override;
+
+private:
+};
+
+}
+}
---
+++ libfunzel/src/nn/Graph.cpp
@@ -0,0 +1,24 @@
+#include <funzel/nn/Graph.hpp>
+
+using namespace funzel;
+using namespace nn;
+
+Tensor Graph::forward(const Tensor& input)
+{
+	return Tensor();
+}
+
+Tensor Graph::backward(const Tensor& input)
+{
+	return Tensor();
+}
+
+void Graph::to(const std::string& device)
+{
+
+}
+
+void Graph::defaultInitialize()
+{
+
+}
