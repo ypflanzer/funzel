e2212bf478986e641612b229119fae4790db802b
Yannick Pflanzer <ypflanzer@outlook.com>
2024-01-03 10:35:52 +0100

    Unravel matrices before checking their equality in tests.

--- libfunzel/tests/TestUtils.hpp
+++ libfunzel/tests/TestUtils.hpp
@@ -3,9 +3,13 @@
 #define EXPECT_TENSOR_EQ(t1, t2) \
 ASSERT_EQ((t1).shape, (t2).shape); \
 ASSERT_EQ((t1).dtype, (t2).dtype); \
-	for(size_t i = 0; i < (t1).size(); i++) \
 	{ \
-		float* cdata = (float*) (t1).data(i*sizeof(float)); \
-		float* edata = (float*) (t2).data(i*sizeof(float)); \
-		EXPECT_EQ(*cdata, *edata); \
+		auto t1cont = (t1).unravel(); \
+		auto t2cont = (t2).unravel(); \
+		for(size_t i = 0; i < (t1).size(); i++) \
+		{ \
+			float* cdata = (float*) (t1cont).data(i*sizeof(float)); \
+			float* edata = (float*) (t2cont).data(i*sizeof(float)); \
+			EXPECT_EQ(*cdata, *edata); \
+		}\
 	}
